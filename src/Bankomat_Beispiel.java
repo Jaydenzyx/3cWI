09eee669b56534766308d0d51bb2c39d90ae5802
Jaydenzyx <jayden.pribozic@student.htldornbirn.at>
2024-10-02 09:31:38 +0200

    End_of_lesson

---
+++ src/Bankomat_Beispiel.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Bankomat_Beispiel {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int seletcion =scanner.nextInt();
+
+
+        boolean isfinished = false;
+
+        int Kontostand = 0;
+        while (!isfinished){
+
+            System.out.println("1 Einzahlen");
+            System.out.println("2 Abheben");
+            System.out.println("3 Kontostand");
+            System.out.println("4 Beenden");
+
+            if (seletcion == 1);
+            System.out.println("Wie viel möchten Sie gerne Einzahlen");;
+
+        }
+
+
+    }
+}
