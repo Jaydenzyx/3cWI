b308bcf1eefcb35b2a91bf1a149642efc8b1f5e9
Jaydenzyx <jayden.pribozic@student.htldornbirn.at>
2024-10-02 09:07:47 +0200

    While_Aufgabe_1_finished

---
+++ src/While_Aufgabe_1.java
@@ -0,0 +1,31 @@
+import com.sun.source.tree.WhileLoopTree;
+
+import java.util.Random;
+
+public class While_Aufgabe_1 {
+
+    public static void main(String[] args) {
+
+        boolean isfinished = false;
+        int sum = 0;
+
+        while(!isfinished){
+
+            Random random = new Random();
+
+            int randomNumber = random.nextInt(21)+10;
+            sum+=randomNumber;
+
+                if (randomNumber == 15 || randomNumber == 25){
+                    isfinished = true ;
+                }
+
+            if (randomNumber == 15 || randomNumber == 25){
+                System.out.println(sum);
+            }
+
+        }
+
+    }
+
+}
