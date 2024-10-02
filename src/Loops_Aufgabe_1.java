e01d41876a7c4382c5356cd71f2b4ab7753f0cd2
Jaydenzyx <jayden.pribozic@student.htldornbirn.at>
2024-10-02 08:08:24 +0200

    Loops_Aufgabe_1_finished

--- src/Loops_Aufgabe_1.java
+++ src/Loops_Aufgabe_1.java
@@ -3,9 +3,13 @@ public class Loops_Aufgabe_1 {
 
        //Zähle in einem For-Loop die Zahlen von 1 bis 100 (inklusive) zusammen
 
+        int sum = 0;
+
         for (int i = 1;i<=100; i ++){
-            System.out.println(i);
+            sum+=i;
         }
+
+        System.out.println(sum);
 
     }
 }
