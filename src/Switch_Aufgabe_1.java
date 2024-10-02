2ca6909d9cd85cf7fc2b94c2e83df26b0bd509ba
Jaydenzyx <jayden.pribozic@student.htldornbirn.at>
2024-10-02 08:03:32 +0200

    Switch_Aufgabe_1_finished

---
+++ src/Switch_Aufgabe_1.java
@@ -0,0 +1,49 @@
+import java.util.Random;
+
+public class Switch_Aufgabe_1 {
+
+    public static void main(String[] args) {
+
+
+
+        Random random = new Random();
+
+        int randomNumber = random.nextInt(6)+5;
+
+// Wenn der Wert 10 ist gib aus Ten
+
+        switch (randomNumber){
+
+            case 5:
+                System.out.println("5");
+                break;
+
+            case 6:
+                System.out.println("6");
+                break;
+
+            case 7:
+                System.out.printf("7");
+                break;
+
+            case 8:
+                System.out.println("8");
+                break;
+
+            case 9:
+                System.out.println("9");
+                break;
+
+            case 10:
+                System.out.println("10");
+                break;
+
+        }
+// Wenn der Wert 9 ist gib aus Nine
+
+// Wenn der Wert 8 ist gib aus Eight
+
+// etc.
+
+        }
+}
