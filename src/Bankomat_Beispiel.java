5db4630c935b22646f284da6323894e4b534054a
Jaydenzyx <jayden.pribozic@student.htldornbirn.at>
2024-10-03 09:24:33 +0200

    Bankomat_Beispiel_finished

--- src/Bankomat_Beispiel.java
+++ src/Bankomat_Beispiel.java
@@ -1,28 +1,81 @@
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Bankomat_Beispiel {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        int seletcion =scanner.nextInt();
-
 
         boolean isfinished = false;
 
         int Kontostand = 0;
-        while (!isfinished){
+        while (!isfinished) {
 
             System.out.println("1 Einzahlen");
             System.out.println("2 Abheben");
             System.out.println("3 Kontostand");
             System.out.println("4 Beenden");
+            int seletcion = scanner.nextInt();
+            if (seletcion == 1) {
+                System.out.println("Geben sie den Betrag ein den sie Einzahlen möchten");
+                ;
 
-            if (seletcion == 1);
-            System.out.println("Wie viel möchten Sie gerne Einzahlen");;
+                if(scanner.hasNextInt()){
+                    int Einzahlen = scanner.nextInt();
+
+                    if (Einzahlen>0){
+                        Kontostand+=Einzahlen;
+                        System.out.println("erfolgreich" + " " + Kontostand + "€");
+                    }
+
+                    else{
+                        System.out.println("Geben sie bitte eine positive Zahl ein");
+                    }
+                }
+
+                else {
+                    System.out.println("geben sie bitte eine gültige Zahl ein");
+                    scanner.next();
+                }
+
+            }
+
+
+            if(seletcion == 2){
+                System.out.println("Geben Sie den Betrag ein, den Sie abheben möchten:");
+
+                if(scanner.hasNextInt()){
+
+                    int Abheben = scanner.nextInt();
+
+                    if (Abheben>0){
+                        Kontostand-= Abheben;
+                        System.out.println("Erfolgreich" +" " + Kontostand + "€");
+                    }
+
+                    else {
+                        System.out.println("Geben sie bitte eine Positive Zahl ein");
+                    }
+
+                }
+
+                else {
+                    System.out.println("Eingabe Ungültig geben sie bitte eine gültige Zahl ein");
+                    scanner.next();
+                }
+
+            }
+
+            if (seletcion == 3){
+                System.out.println("Ihr Kontostand Beträgt:" + Kontostand + "€");
+            }
+
+            if (seletcion == 4){
+                isfinished = true;
+            }
 
         }
-
-
     }
 }
+
