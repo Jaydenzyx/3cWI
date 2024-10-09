c03b996807081d83d36051f4d0db0f43d5023277
Jaydenzyx <jayden.pribozic@student.htldornbirn.at>
2024-10-09 08:59:09 +0200

    While_Aufgabe_1_finished

--- src/While_Aufgabe_1.java
+++ src/While_Aufgabe_1.java
@@ -9,17 +9,17 @@ public class While_Aufgabe_1 {
         boolean isfinished = false;
         int sum = 0;
 
-        while(!isfinished){
+        while (!isfinished) {
 
             Random random = new Random();
 
-            int randomNumber = random.nextInt(21)+10;
-            sum+=randomNumber;
+            int randomNumber = random.nextInt(21) + 10;
+            sum += randomNumber;
 
-                if (randomNumber == 15 || randomNumber == 25){
-                    System.out.println(sum);
-                    isfinished = true ;
-                }
+            if (randomNumber == 15 || randomNumber == 25) {
+                System.out.println(sum);
+                isfinished = true;
+            }
         }
     }
 
