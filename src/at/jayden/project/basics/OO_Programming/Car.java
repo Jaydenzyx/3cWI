5b4f2221fff17cd4e60855c5c455c977ea54c467
Jaydenzyx <jayden.pribozic@student.htldornbirn.at>
2024-11-14 09:25:21 +0100

    end of lesson

--- src/at/jayden/project/basics/OO_Programming/Car.java
+++ src/at/jayden/project/basics/OO_Programming/Car.java
@@ -7,13 +7,43 @@ public class Car {
     public String brand;
     public String serialNumber;
     private String color;
+    public int MaxFuel;
+    public int amountOfRepetitions;
 
 
-    public void drive (){
+    public void drive() {
         this.fuelAmount = this.fuelAmount - this.fuelConsumption;
         System.out.println("I am driving");
     }
 
+    public void brake() {
+        System.out.println("Ich bremse");
+    }
 
+    public void turboBoost() {
+        if (this.fuelAmount > this.MaxFuel * 0.10) {
+            System.out.println("SuperBoostMode");
+        } else {
+            System.out.println("Not enough Fuel to go to Superboost");
+        }
+    }
+
+    public void honk() {
+
+        for (int i = 0; i < this.amountOfRepetitions; i++) {
+            System.out.println("Tuuutuuu");
+        }
+
+    }
+
+    public void getRemainingRange(){
+        int RemainingRange = this.fuelAmount/this.fuelConsumption*100;
+        System.out.println(RemainingRange + "km ");
+    }
 
 }
+
+
+
+
+
--- src/at/jayden/project/basics/OO_Programming/main.java
+++ src/at/jayden/project/basics/OO_Programming/main.java
@@ -9,22 +9,27 @@ public class main {
         c1.fuelConsumption = 7;
         c1.serialNumber = "1234";
         c1.fuelAmount = 70;
+        c1.MaxFuel = 100;
+        c1.amountOfRepetitions = 5;
 
         Car c2 = new Car();
         c2.brand = "Mercedes";
         c2.fuelConsumption = 6;
         c2.serialNumber = "12345";
         c2.fuelAmount = 20;
+        c2.MaxFuel = 130;
+        c2.amountOfRepetitions = 5;
 
 
         System.out.println(c2.fuelAmount);
         c2.drive();
         System.out.println(c2.fuelAmount);
-
+        c1.brake();
+        c1.turboBoost();
+        c2.honk();
+        c1.getRemainingRange();
 
     }
-
-
 
 
 }
