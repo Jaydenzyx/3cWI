5e0e4f0aca7b1943ba405091ded918299fd6e195
Jaydenzyx <jayden.pribozic@student.htldornbirn.at>
2025-02-06 08:45:48 +0100

    Static_final

---
+++ src/at/jayden/project/basics/finaltest/Staticfinal.java
@@ -0,0 +1,12 @@
+package at.jayden.project.basics.finaltest;
+
+public class Staticfinal {
+
+        public static final double PI = 3.14159;
+
+        public static void main(String[] args) {
+            System.out.println("Der Wert von PI ist: " + PI);
+        }
+    }
+
+
---
+++ src/at/jayden/project/basics/finaltest/test.java
@@ -0,0 +1,13 @@
+package at.jayden.project.basics.finaltest;
+
+public class test {
+
+    public static void main(String[] args) {
+
+        final int ZAHL = 10;
+        System.out.println(ZAHL);
+
+        // ZAHL = 20;
+    }
+
+}
